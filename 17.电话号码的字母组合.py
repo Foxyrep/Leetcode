2925a27eb7ab0f2a5065b5148edc18de1d0f1f08
zwx1185 <15319350358@163.com>
2021-10-25 00:09:11 +0800

    1

---
+++ 17.电话号码的字母组合.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=17 lang=python3
+#
+# [17] 电话号码的字母组合
+#
+
+# @lc code=start
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        Table = [
+            ['a', 'b', 'c'],
+            ['d', 'e', 'f'],
+            ['g', 'h', 'i'],
+            ['j', 'k', 'l'],
+            ['m', 'n', 'o'],
+            ['p', 'q', 'r', 's'],
+            ['t', 'u', 'v'],
+            ['w', 'x', 'y', 'z'],
+            ]
+        s = ''
+        for i in range(len(digits)):
+            num = digits[i]
+            arr = Table[num-1]
+            for j in range(len(arr)):
+                char = arr[j]
+                s = s+char
+
+        
+# @lc code=end
+
